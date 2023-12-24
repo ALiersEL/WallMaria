cff5dd936e81eb5eb1d2ec7f5f7b17d93ee409f4
yhzx233 <2058777361@qq.com>
2023-12-25 01:28:21 +0800

    feat: crop search

--- wallmaria-backend/main.py
+++ wallmaria-backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
 
 from models import Post, PostInDB, UploadResponse
+from bson import ObjectId
 
 from pymilvus import Collection
 
@@ -22,6 +23,7 @@ app = FastAPI()
 # MongoDB
 mongodb = database.connect_mongo()
 post_mongo = mongodb["posts"]
+search_mongo = mongodb["searches"]
 
 # Milvus
 database.connect_milvus()
@@ -53,30 +55,7 @@ async def get_posts(limit: int = 10, skip: int = 0):
     posts = await post_mongo.find({"rating": {"$in": ["g"]}}).sort("score", -1).limit(limit).skip(skip).to_list(length=limit)
     return posts
 
-@app.get("/search_by_text", response_model=list[Post])
-async def search_images_by_text(text: str, top_k: int = 5):
-    """
-    根据文本查询相似的图片。
-
-    参数:
-        text (str): 查询用的文本。
-        top_k (int): 返回的相似图片数量。
-
-    返回:
-        list: 相似图片的 image_id 列表。
-    """
-
-    print("Searching for images similar to:", text)
-    # 将文本编码为向量
-    text_features = extract_text_features(clip_model, [text], device=device)
-
-    image_ids = search_similar_images(post_milvus, text_features, 1, top_k)
-
-    posts = await get_posts_from_ids(image_ids)
-
-    return posts
-
-@app.post("/search_by_image", response_model=list[Post])
+@app.post("/search_by_image_dep", response_model=list[Post])
 async def search_images_by_image(file: UploadFile = File(...), top_k: int = 5):
     """
     根据图片查询相似的图片。
@@ -108,7 +87,7 @@ async def search_images_by_image(file: UploadFile = File(...), top_k: int = 5):
 
     return posts
 
-@app.post("/upload_text", response_model=UploadResponse)
+@app.post("/upload_text_dep", response_model=UploadResponse)
 async def upload_text(text: str):
     """
     上传文本并缓存特征，返回 token。
@@ -125,7 +104,7 @@ async def upload_text(text: str):
 
     return {"token": token}
 
-@app.post("/upload_image", response_model=UploadResponse)
+@app.post("/upload_image_dep", response_model=UploadResponse)
 async def upload_image(file: UploadFile = File(...)):
     """
     上传图片并缓存特征，返回 token。
@@ -147,7 +126,7 @@ async def upload_image(file: UploadFile = File(...)):
 
     return {"token": token}
 
-@app.get("/get_results", response_model=list[Post])
+@app.get("/get_results_dep", response_model=list[Post])
 async def get_results(token: str, page: int = 1, page_size: int = 10):
     """
     使用 token 获取搜索结果的不同页。
@@ -162,9 +141,77 @@ async def get_results(token: str, page: int = 1, page_size: int = 10):
 
     # 在 Milvus 中搜索相似的图片
     image_ids = search_similar_images(post_milvus, features, page, page_size)
-
     posts = await get_posts_from_ids(image_ids)
+    return posts
 
+@app.post("/search_by_text", response_model=list[Post])
+async def search_by_text(text: str, page: int = 1, page_size: int = 10):
+    """
+    根据文本搜索相似的图片，并缓存文本特征。
+    """
+    # 使用文本内容作为 Redis 缓存的键
+    cache_key = f"text_{text}"
+    cached_features = await redis_client.get(cache_key)
+
+    if cached_features is None:
+        # 提取文本特征
+        features = extract_text_features(clip_model, [text], device=device)
+
+        # 将特征保存到 Redis
+        await redis_client.set(cache_key, json.dumps(features.tolist()), ex=3600)  # 例如，缓存 1 小时
+    else:
+        # 使用缓存的特征
+        features = json.loads(cached_features)
+
+    # 在 Milvus 中搜索相似的图片
+    image_ids = search_similar_images(post_milvus, features, page, page_size)
+    posts = await get_posts_from_ids(image_ids)
+    return posts
+
+@app.post("/upload_image", response_model=UploadResponse)
+async def upload_image(file: UploadFile = File(...)):
+    """
+    上传图片并保存，返回一个唯一的 token。
+    """
+    image_data = await file.read()
+    res = await search_mongo.insert_one({"image_data": image_data})
+    token = str(res.inserted_id)
+    return {"token": token}
+
+@app.post("/search_by_crop", response_model=list[Post])
+async def search_by_crop(token: str, left: int, top: int, width: int, height: int, page: int = 1, page_size: int = 10):
+    """
+    根据裁剪参数和 token 搜索相似的图片。
+    """
+    # 检查 Redis 缓存
+    cache_key = f"{token}_{left}_{top}_{width}_{height}"
+    cached_features = await redis_client.get(cache_key)
+
+    if cached_features is None:
+        # 从 MongoDB 获取图片
+        try:
+            image_record = await search_mongo.find_one({"_id": ObjectId(token)})
+        except:
+            raise HTTPException(status_code=404, detail="Invalid token")
+
+        if not image_record:
+            raise HTTPException(status_code=404, detail="Image not found")
+
+        image = Image.open(io.BytesIO(image_record["image_data"]))
+        # 执行裁剪和特征提取
+        cropped_image = image.crop((left, top, left + width, top + height))
+        transformed_image = transform(cropped_image)
+        features = extract_image_features(clip_model, [transformed_image], device=device)
+
+        # 将特征保存到 Redis
+        await redis_client.set(cache_key, json.dumps(features.tolist()), ex=3600)  # 例如，缓存 1 小时
+    else:
+        # 使用缓存的特征
+        features = json.loads(cached_features)
+
+    # 在 Milvus 中搜索相似的图片
+    image_ids = search_similar_images(post_milvus, features, page, page_size)
+    posts = await get_posts_from_ids(image_ids)
     return posts
 
 async def get_posts_from_ids(image_ids):
