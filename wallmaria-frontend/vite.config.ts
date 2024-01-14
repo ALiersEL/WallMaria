c522b8a000a5a904e1096cf709b73e7506ab7ba5
ALiersEL <johnathansteinfeld@gmail.com>
2024-01-15 04:01:22 +0800

    feat: add domain to deployed backend server

--- wallmaria-frontend/vite.config.ts
+++ wallmaria-frontend/vite.config.ts
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [ vue(),],
-  server: {
-    port: 5173,
-    proxy: { // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
-      // 正则表达式写法
-      '^/api': {
-        target: 'http://58.37.44.136:7000', // 后端服务实际地址
-        ws: true,
-        changeOrigin: true, //开启代理
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-});
+export default ({ mode }) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    return defineConfig({
+        plugins: [ vue(),],
+        server: {
+            port: 5173,
+            proxy: { // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
+                // 正则表达式写法
+                '^/api': {
+                    target: process.env.VITE_APP_BACKEND_URL,
+                    ws: true,
+                    changeOrigin: true, //开启代理
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
+            }
+        },
+    });
+}
 
